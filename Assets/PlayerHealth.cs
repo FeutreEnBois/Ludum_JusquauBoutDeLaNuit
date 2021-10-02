966706228a9fba1bcd01b3161a83f37f912c7ac9
Maxime Derosier <derosier.maxime@gmail.com>
2021-10-03 00:35:04 +0200

    PlayerHealth

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        CheckLife(currentHealth);
+    }
+
+    private void CheckLife(float currentHealth)
+    {
+        if (currentHealth <= 0)
+        {
+            Debug.Log("player is dead");
+        }
+    }
+}
